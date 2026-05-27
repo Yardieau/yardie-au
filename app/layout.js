3e020a00c133d317f0b99daea67b8cf33e50c6dc
Yardieau <kristybotros.cmns@gmail.com>
2026-05-27 19:07:10 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
-test
+import './globals.css'
+
+export const metadata = {
+  title: 'Yardie.au | Australia’s Local Online Marketplace',
+  description: 'Buy it. Sell it. Yard it.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
