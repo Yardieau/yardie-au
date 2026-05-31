95763ea28bf25b35525a72c5367456fae88c6ac1
Yardieau <kristybotros.cmns@gmail.com>
2026-05-31 16:32:27 +1000

    Add layout code

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Yardie.au",
+  description: "Australia's Local Marketplace",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
